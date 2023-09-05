d3818c7c90b4bd6ea3af59b37b2960d21f5c8966
Eugen <eugen252009@gmail.com>
2023-09-05 21:32:09 +0200

    Zum Commit vorgemerkte Änderungen: 	geändert:       main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
+
+
 fn main() {
+    // creating a local host address and port to create final Endpoint
+
+    const HOST : &str ="127.0.0.1";
+    const PORT : &str="8477";
+    let end_point : String = HOST.to_owned()+ ":" + PORT;
+    let listener = TcpListener::bind(end_point).unwrap();
+    println!("Webserver is listening on Port {}",PORT);
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established!");
+    }
+
     println!("Hello, world!");
 }
